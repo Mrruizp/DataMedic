b9ac282f606a14c1f818128fc9896678811991f3
Mrruizp <renzo.ruizp@outlook.com>
2020-05-07 00:04:33 -0500

    primer commit, con un avance en la interfaz de usuario en la página principal

---
+++ view/index.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>StemaClinico | Inicio</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php include_once 'estilos.view.php'; ?>
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <?php //include_once './menu-arriba.admin.view.php'; ?>
+
+  <!-- =============================================== -->
+  <!-- Left side column. contains the sidebar -->
+  <?php //include_once 'menu-izquierda.admin.view.php'; ?>
+
+  <!-- =============================================== -->
+
+  
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="main-header">
+    <!-- Content Header (Page header) -->
+    <!--<div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-12">
+            <h1 class="m-0 text-center text-info">DataMedic</h1>
+          </div>
+        </div>
+      </div>
+    </div>-->
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+          <div class="col-xs-1">
+            <!-- small box -->
+            <div class="">
+              <div class="inner">
+                <img src="../images/LogoDental.jpg" class="img img-responsive center-block" width="150">
+              </div>
+            </div>
+          </div>
+          <!-- ./col -->
+          <div class="col-lg-5">
+            <!-- small box -->
+              <div class="card-header col-lg-10">
+                  <h3 class="text-bold">
+                    Estetica Dental Alcedo
+                  </h3>
+              </div>
+              <div><br/>
+                <div class="col-md-12 col-sm-4">
+                  <h6>
+                    <ion-icon name="location-outline"></ion-icon>
+                    Av las Nazarenas 186, Santiago de Surco
+                  </h6>
+                </div>
+              </div>
+                <div class="col-md-12 col-sm-4">
+                  <h6>
+                    <ion-icon name="phone-portrait-outline"></ion-icon>
+                     924869860
+                  </h6>
+                </div>
+          </div>
+          <!-- ./col -->
+        </div>
+        <!-- /.row -->
+        <!-- Main row -->
+        <div class="row">
+          <!-- Left col -->
+          <section class="col-lg-6">
+            <!-- Custom tabs (Charts with tabs)-->
+            <div class="card">
+              <div class="card-header text-center bg-info">
+                <h3>
+                  <!--<ion-icon name="calendar-outline"></ion-icon>-->
+                  Agenda tu cita
+                </h3>
+              </div><!-- /.card-header -->
+              <div class="form-horizontal">
+                <div class="card-body">
+                  <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-2 col-form-label">Especialidad:</label>
+                    <div class="row">
+                      <div class="col-sm-12">
+                      <!-- select -->
+                          <div class="form-group">
+                            <select class="form-control">
+                              <option>Estética Dental & Diseño de Sonrisa</option>
+                              <option>option 2</option>
+                              <option>option 3</option>
+                              <option>option 4</option>
+                              <option>option 5</option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                    <label for="inputEmail3" class="col-sm-2 col-form-label">Doctor:</label>
+                    <div class="row">
+                      <div class="col-sm-12">
+                      <!-- select -->
+                          <div class="form-group">
+                            <select class="form-control">
+                              <option>Maritza Alcedo</option>
+                              <option>option 2</option>
+                              <option>option 3</option>
+                              <option>option 4</option>
+                              <option>option 5</option>
+                            </select>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              
+              <div class="card-body">
+                <label for="inputEmail3" class="col-sm-2 col-form-label">Fecha y hora:</label>
+                <table class="table table-bordered">
+                  <thead>                  
+                    <tr style="background-color: #ededed; height:25px;">
+                      <th style="width: 10px">#</th>
+                      <th>Fecha</th>
+                      <th>Hora</th>
+                      <th>Estado</th>
+                      <th style="width: 40px">Guardar</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1.</td>
+                      <td>Lunes 11 de Mayo</td>
+                      <td>
+                        10:00
+                      </td>
+                      <td>disponible</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>2.</td>
+                      <td>Lunes 11 de Mayo</td>
+                      <td>
+                        10:30
+                      </td>
+                      <td>disponible</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>3.</td>
+                      <td>Lunes 11 de Mayo</td>
+                      <td>
+                        11:00
+                      </td>
+                      <td>disponible</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>4.</td>
+                      <td>Lunes 11 de Mayo</td>
+                      <td>
+                        11:30
+                      </td>
+                      <td>disponible</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>5.</td>
+                      <td>Lunes 11 de Mayo</td>
+                      <td>
+                        12:00
+                      </td>
+                      <td>disponible</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>6.</td>
+                      <td>Lunes 11 de Mayo</td>
+                      <td>
+                        12:30
+                      </td>
+                      <td>disponible</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>7.</td>
+                      <td>Lunes 11 de Mayo</td>
+                      <td>
+                        2:00
+                      </td>
+                      <td>disponible</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>8.</td>
+                      <td>Lunes 11 de Mayo</td>
+                      <td>
+                        2:30
+                      </td>
+                      <td>disponible</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>9.</td>
+                      <td>Lunes 11 de Mayo</td>
+                      <td>
+                        3:00
+                      </td>
+                      <td>disponible</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>10.</td>
+                      <td>Lunes 11 de Mayo</td>
+                      <td>
+                        3:30
+                      </td>
+                      <td>disponible</td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td>11.</td>
+                      <td>Lunes 11 de Mayo</td>
+                      <td>
+                        4:00
+                      </td>
+                      <td>disponible</td>
+                      <td></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
+              </div>
+
+            </div>
+            <!-- /.card -->
+          </section>
+          <section class="col-lg-6">
+            <div class="col-lg-12">
+            <!-- small box -->
+              <div class="card-header small-box col-lg-10">
+                  <h5 class="text-bold">
+                    Más información
+                  </h5>
+              </div>
+              <br/>
+                <div class="col-md-12 col-sm-4">
+                  <h6>
+                    <ion-icon name="earth-outline"></ion-icon>
+                    <b>Web: </b> http://alfirk.org.pe
+                  </h6>
+                </div>
+                <div class="col-md-12 col-sm-4">
+                  <h6>
+                    <ion-icon name="time-outline"></ion-icon>
+                     <b>Atención: </b> Lunes-Viernes
+                  </h6>
+                </div>
+            </div><br/>
+            <div class="col-lg-12">
+            <!-- small box -->
+              <div class="card-header small-box col-lg-10">
+                  <h5 class="text-bold">
+                    Especialidades
+                  </h5>
+              </div>
+              <br/>
+                <div class="col-md-12 col-sm-4">
+                  <h6>
+                    <ion-icon name="checkmark-outline"></ion-icon>
+                    Endodoncia
+                  </h6>
+                </div>
+                <div class="col-md-12 col-sm-4">
+                  <h6>
+                    <ion-icon name="checkmark-outline"></ion-icon>
+                     Estética Dental & Diseño de Sonrisa
+                  </h6>
+                </div>
+                <div class="col-md-12 col-sm-4">
+                  <h6>
+                    <ion-icon name="checkmark-outline"></ion-icon>
+                    Implantología
+                  </h6>
+                </div>
+                <div class="col-md-12 col-sm-4">
+                  <h6>
+                    <ion-icon name="checkmark-outline"></ion-icon>
+                     Ortodoncia, cirugía, rehabilitación oral
+                  </h6>
+                </div>
+            </div>
+          </section>
+        </div>
+        <!-- /.row (main row) -->
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <?php include_once 'pie.view.php'; ?>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+<?php include_once './scripts.view.php'; ?>
+</body>
+</html>
