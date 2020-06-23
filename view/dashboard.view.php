13f5c971dc7eb13632ef53bb0fa65f2faf73f1e8
Mrruizp <renzo.ruizp@outlook.com>
2020-06-23 01:24:31 -0500

    se agregó la página del dashboard, diseño

--- view/dashboard.view.php
+++ view/dashboard.view.php
@@ -7,139 +7,386 @@ require_once '../controller/perfil.usuario.leer.datos.controller.php';
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
+<head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="icon" href="../images/datamedic.png">
-        <title> Data Medic | Inicio</title>
+        <title> Data Medic | Tablero</title>
         <!-- Tell the browser to be responsive to screen width -->
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <?php include_once 'estilos.view.php'; ?>
     </head>
+<body class="hold-transition skin-purple-light sidebar-mini">
+<div class="wrapper">
+  <?php include_once './menu-arriba.admin.view.php'; ?>
 
-    <body class="hold-transition skin-purple-light sidebar-mini">
-        <!-- Site wrapper -->
-        <div class="wrapper">
+  <!-- =============================================== -->
 
-            <?php include_once './menu-arriba.admin.view.php'; ?>
+  <!-- Left side column. contains the sidebar -->
+  <?php include_once 'menu-izquierda.admin.view.php'; ?>
 
-            <!-- =============================================== -->
+  <!-- =============================================== -->
 
-            <!-- Left side column. contains the sidebar -->
-            <?php include_once 'menu-izquierda.admin.view.php'; ?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>Tablero</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+              <li class="breadcrumb-item active">Tablero</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
 
-            <!-- =============================================== -->
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-6">
+            <!-- BAR CHART -->
+            <div class="card card-success">
+              <div class="card-header">
+                <h3 class="card-title">Número de citas, según su estado</h3>
 
-            <div class="content-wrapper">
-                <!-- Content Header (Page header) -->
-                <section class="content-header">
-                  <div class="container-fluid">
-                    <div class="row mb-2">
-                      <div class="col-sm-6">
-                        <h1>Módulo Tratamiento</h1>
-                      </div>
-                      <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                          <li class="breadcrumb-item"><a href="menu.principal.view.php">Inicio</a></li>
-                          <li class="breadcrumb-item active">Módulo de Tratamiento</li>
-                        </ol>
-                      </div>
-                    </div>
-                  </div><!-- /.container-fluid -->
-                </section>
-                <!-- Main content -->
-                <section class="content">
-                  <div class="container-fluid">
-                    <div class="row">
-                      <!-- left column -->
-                      <div class="col-md-12">
-                        <!-- general form elements -->
-                        <div class="card card-default">
-                          <div class="card-header">
-                            <h3 class="card-title">Tratamiento</h3>
-                          </div>
-                          <!-- /.card-header -->
-                          <!-- form start -->
-                          
-                            <div class="card-body">
-                              <div class="form-group">
-                                <button type="button" class="btn btn-outline-info btn-sm" data-toggle="modal" data-target="#myModal" id="btnagregar"><ion-icon name="calendar-outline"></ion-icon> Agregar Tratamiento</button>
-                              </div>
-                              <div class="form-group">
-                               <div id="listado" class="table table-responsive"></div>
-                                
-                              </div>
-                            </div>
-                         
-                        </div>
-                        <!-- /.card -->
-                            </div>
-                        </div>
-                    </div>
-
-              </section>
-              <form id="frmgrabar" class="form-horizontal">
-                <div class="modal fade" id="myModal">
-                  <div class="modal-dialog modal-lg">
-                    <div class="modal-content">
-                      <div class="modal-header bg-info">
-                        <h4 class="modal-title" id = "titulomodalTratamiento">Registrar Tratamiento</h4>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span></button>
-                      </div>
-                      <div class="modal-body">
-                        <div class="row">
-                            <div class="col-2">
-                                <p>
-                                    <input type="hidden" value="" id="txtTipoOperacion" name="txtTipoOperacion">
-                                    Código <input type="text" 
-                                                  name="txtCodigo" 
-                                                  id="txtCodigo" 
-                                                  class="form-control input-sm" 
-                                                  readonly="true">
-                                </p>
-                            </div>
-                            <div class="col-8">
-                                <p>
-                                    
-                                    Nombre del Tratamiento<input type="text" 
-                                                  name="txtTratamiento" 
-                                                  id="txtTratamiento" 
-                                                  class="form-control input-sm">
-                                </p>
-                            </div>
-                          </div>
-                      </div>
-                      <div class="modal-footer justify-content-between">
-                        <button type="button" class="btn btn-danger" data-dismiss="modal" id="btncerrar">Close</button>
-                        <button type="submit" class="btn btn-outline-info"><ion-icon name="save-outline"></ion-icon>  Registrar</button>
-                      </div>
-                    </div>
-                    <!-- /.modal-content -->
-                  </div>
-                  <!-- /.modal-dialog -->
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
                 </div>
-                <!-- /.modal -->
-              </form>
-              
-            <!--<?php include_once 'pie.view.php'; ?>-->
+              </div>
+              <div class="card-body">
+                <div class="chart">
+                  <canvas id="barChart" style="height:230px; min-height:230px"></canvas>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
 
-            <!-- Control Sidebar -->
-            <?php // include_once 'opciones-derecha.view.php'; ?>
-            <!-- /.control-sidebar -->
-            <div class="control-sidebar-bg"></div>
-         
-        <!-- ./wrapper -->
-        <?php include_once 'scripts.view.php'; ?>
-        <script src="js/gestionarTratamiento.js" type="text/javascript"></script>
-        
-    <!--
-        <script src="js/convocatoriaVigente.js" type="text/javascript"></script>
-        <script src="js/convocatoriaConcluida.js" type="text/javascript"></script>
-        <script src="js/reporte.resultadoCurriculo.js" type="text/javascript"></script>
-        <script src="js/reporte.resultadoPruebas.js" type="text/javascript"></script>
-        <script src="js/reporte.resultadoFinal.js" type="text/javascript"></script>
-    -->
+            <!-- DONUT CHART -->
+            <div class="card card-danger">
+              <div class="card-header">
+                <h3 class="card-title">Número de citas confirmadas para cada consultorio, en el presente mes y año</h3>
 
-    </body>
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <canvas id="donutChart" style="height:230px; min-height:230px"></canvas>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+            <!-- PIE CHART -->
+            <div class="card card-danger">
+              <div class="card-header">
+                <h3 class="card-title">Pie Chart</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <canvas id="pieChart" style="height:230px; min-height:230px"></canvas>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+          </div>
+          <!-- /.col (LEFT) -->
+          <div class="col-md-6">
+            <!-- LINE CHART -->
+            <div class="card card-info">
+              <div class="card-header">
+                <h3 class="card-title">Line Chart</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <div class="chart">
+                  <canvas id="lineChart" style="height:250px; min-height:250px"></canvas>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+            
+
+            <!-- STACKED BAR CHART -->
+            <div class="card card-success">
+              <div class="card-header">
+                <h3 class="card-title">Stacked Bar Chart</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                <div class="chart">
+                  <canvas id="stackedBarChart" style="height:230px; min-height:230px"></canvas>
+                </div>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+          </div>
+          <!-- /.col (RIGHT) -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <?php include_once 'pie.view.php'; ?>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Add Content Here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+<?php include_once 'scripts.view.php'; ?>
+<!-- jQuery -->
+<script src="../util/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../util/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="../util/plugins/chart.js/Chart.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../util/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../util/dist/js/demo.js"></script>
+<!-- page script -->
+<script>
+  $(function () {
+    /* ChartJS
+     * -------
+     * Here we will create a few charts using ChartJS
+     */
+
+    //--------------
+    //- AREA CHART -
+    //--------------
+
+    // Get context with jQuery - using jQuery's .get() method.
+    var areaChartCanvas = $('#barChart').get(0).getContext('2d')
+
+    var areaChartData = {
+      labels  : ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junino', 'Julio', 'Agosto', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+      datasets: [
+        {
+          label               : 'Citas en Proceso',
+          backgroundColor     : '#f39c12',
+          borderColor         : '#f39c12',
+          pointRadius          : false,
+          pointColor          : '#3b8bba',
+          pointStrokeColor    : '#f39c12',
+          pointHighlightFill  : '#fff',
+          pointHighlightStroke: '#f39c12',
+          data                : [28, 48, 40, 19, 86, 27, 90]
+        },
+        {
+          label               : 'Citas en Confirmadas',
+          backgroundColor     : '#3c8dbc',
+          borderColor         : '#3c8dbc',
+          pointRadius         : false,
+          pointColor          : '#3c8dbc',
+          pointStrokeColor    : '#3c8dbc',
+          pointHighlightFill  : '#3c8dbc',
+          pointHighlightStroke: '#3c8dbc',
+          data                : [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+          label               : 'Citas en Atendidas',
+          backgroundColor     : '#00a65a',
+          borderColor         : '#00a65a',
+          pointRadius         : false,
+          pointColor          : '00a65a',
+          pointStrokeColor    : '#00a65a',
+          pointHighlightFill  : '#00a65a',
+          pointHighlightStroke: '#00a65a',
+          data                : [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+          label               : 'Citas en Denegadas',
+          backgroundColor     : '#f56954',
+          borderColor         : '#f56954',
+          pointRadius         : false,
+          pointColor          : '#f56954',
+          pointStrokeColor    : '#f56954',
+          pointHighlightFill  : '#f56954',
+          pointHighlightStroke: '#f56954',
+          data                : [65, 59, 80, 81, 56, 55, 40]
+        },
+      ]
+    }
+
+    var areaChartOptions = {
+      maintainAspectRatio : false,
+      responsive : true,
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          gridLines : {
+            display : false,
+          }
+        }],
+        yAxes: [{
+          gridLines : {
+            display : false,
+          }
+        }]
+      }
+    }
+
+    // This will get the first returned node in the jQuery collection.
+    var areaChart       = new Chart(areaChartCanvas, { 
+      type: 'line',
+      data: areaChartData, 
+      options: areaChartOptions
+    })
+
+    //-------------
+    //- LINE CHART -
+    //--------------
+    var lineChartCanvas = $('#lineChart').get(0).getContext('2d')
+    var lineChartOptions = jQuery.extend(true, {}, areaChartOptions)
+    var lineChartData = jQuery.extend(true, {}, areaChartData)
+    lineChartData.datasets[0].fill = false;
+    lineChartData.datasets[1].fill = false;
+    lineChartOptions.datasetFill = false
+
+    var lineChart = new Chart(lineChartCanvas, { 
+      type: 'line',
+      data: lineChartData, 
+      options: lineChartOptions
+    })
+
+    //-------------
+    //- DONUT CHART -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+    var donutData        = {
+      labels: [
+          'Chrome', 
+          'IE',
+          'FireFox', 
+          'Safari', 
+          'Opera', 
+          'Navigator', 
+      ],
+      datasets: [
+        {
+          data: [700,500,400,600,300,100],
+          backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+        }
+      ]
+    }
+    var donutOptions     = {
+      maintainAspectRatio : false,
+      responsive : true,
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    var donutChart = new Chart(donutChartCanvas, {
+      type: 'doughnut',
+      data: donutData,
+      options: donutOptions      
+    })
+
+    //-------------
+    //- PIE CHART -
+    //-------------
+    // Get context with jQuery - using jQuery's .get() method.
+    var pieChartCanvas = $('#pieChart').get(0).getContext('2d')
+    var pieData        = donutData;
+    var pieOptions     = {
+      maintainAspectRatio : false,
+      responsive : true,
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    var pieChart = new Chart(pieChartCanvas, {
+      type: 'pie',
+      data: pieData,
+      options: pieOptions      
+    })
+
+    //-------------
+    //- BAR CHART -
+    //-------------
+    var barChartCanvas = $('#barChart').get(0).getContext('2d')
+    var barChartData = jQuery.extend(true, {}, areaChartData)
+    var temp0 = areaChartData.datasets[0]
+    var temp1 = areaChartData.datasets[1]
+    barChartData.datasets[0] = temp1
+    barChartData.datasets[1] = temp0
+
+    var barChartOptions = {
+      responsive              : true,
+      maintainAspectRatio     : false,
+      datasetFill             : false
+    }
+
+    var barChart = new Chart(barChartCanvas, {
+      type: 'bar', 
+      data: barChartData,
+      options: barChartOptions
+    })
+
+    //---------------------
+    //- STACKED BAR CHART -
+    //---------------------
+    var stackedBarChartCanvas = $('#stackedBarChart').get(0).getContext('2d')
+    var stackedBarChartData = jQuery.extend(true, {}, barChartData)
+
+    var stackedBarChartOptions = {
+      responsive              : true,
+      maintainAspectRatio     : false,
+      scales: {
+        xAxes: [{
+          stacked: true,
+        }],
+        yAxes: [{
+          stacked: true
+        }]
+      }
+    }
+
+    var stackedBarChart = new Chart(stackedBarChartCanvas, {
+      type: 'bar', 
+      data: stackedBarChartData,
+      options: stackedBarChartOptions
+    })
+  })
+</script>
+</body>
 </html>
